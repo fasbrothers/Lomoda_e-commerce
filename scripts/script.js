4bdb69d24441c666bcbf1913a8f5013193bca3da
fasbrothers <nigmatovsanjar00@gmail.com>
2021-07-15 22:44:59 +0500

    added more functions, e.g filtering images

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
-const cityName = document.querySelector('.header__city-button');
+const cityName = document.querySelector(".header__city-button");
+let hash = location.hash.substring(1);
 
-cityName.textContent = localStorage.getItem('lomoda_location') || "Ваш город?"
+cityName.textContent = localStorage.getItem("lomoda_location") || "Ваш город?";
 
-cityName.addEventListener('click', ()=>{
-    const city = prompt('Could you write your city?')
-    cityName.textContent =  city;   
-    localStorage.setItem('lomoda_location', city)
-})
+cityName.addEventListener("click", () => {
+  const city = prompt("Could you write your city?");
+  cityName.textContent = city;
+  localStorage.setItem("lomoda_location", city);
+});
 
 // blocking scroll
 
-const disableScroll = ()=>{
-    const widthScroll = window.innerWidth - document.body.offsetWidth
-    document.body.dbScrollY = window.scrollY
-    document.body.style.cssText = `
+const disableScroll = () => {
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+  document.body.dbScrollY = window.scrollY;
+  document.body.style.cssText = `
         position: fixed;
         width:100%;
         top: ${-window.scrollY}px;
@@ -21,34 +22,121 @@ const disableScroll = ()=>{
         height: 100vh;
         overflow: hidden;
         padding-right: ${widthScroll}px
-    `
-}
-const enableScroll = ()=>{
-    document.body.style.cssText = "";
-    window.scroll({
-        top: document.body.dbScrollY
-    })
-}
+    `;
+};
+const enableScroll = () => {
+  document.body.style.cssText = "";
+  window.scroll({
+    top: document.body.dbScrollY,
+  });
+};
 
 // modal window
 
-const subHeaderCart = document.querySelector('.subheader__cart')
-const cartOverlay = document.querySelector('.cart-overlay')
+const subHeaderCart = document.querySelector(".subheader__cart");
+const cartOverlay = document.querySelector(".cart-overlay");
 
-const cartModalOpen = ()=>{
-    cartOverlay.classList.add('cart-overlay-open')
-    disableScroll()
-}
-const cartModalClose = ()=>{
-    cartOverlay.classList.remove('cart-overlay-open')
-    enableScroll()
-}
-subHeaderCart.addEventListener('click', cartModalOpen)
+const cartModalOpen = () => {
+  cartOverlay.classList.add("cart-overlay-open");
+  disableScroll();
+};
+const cartModalClose = () => {
+  cartOverlay.classList.remove("cart-overlay-open");
+  enableScroll();
+};
 
-cartOverlay.addEventListener('click', (event)=>{
-    const target = event.target;
+// request to database
 
-    if(target.classList.contains('cart__btn-close') || target.matches('.cart-overlay')){
-        cartModalClose()
+const getData = async () => {
+  const data = await fetch("db.json");
+  if (data.ok) {
+    return data.json();
+  } else {
+    throw new Error(data.status + " " + data.statusText);
+  }
+};
+
+const getGoods = (callback, value) => {
+  getData()
+    .then((data) => {
+      if (value) {
+        callback(data.filter((item) => item.category === value));
+      }else{
+        callback(data)
+      }
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+};
+
+getGoods((data) => {
+  // console.warn(data);
+});
+
+subHeaderCart.addEventListener("click", cartModalOpen);
+
+cartOverlay.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (
+    target.classList.contains("cart__btn-close") ||
+    target.matches(".cart-overlay")
+  ) {
+    cartModalClose();
+  }
+});
+
+try {
+  const goodsList = document.querySelector(".goods__list");
+
+  if (!goodsList) {
+    throw "This is not goods list page";
+  }
+
+  const createCard = (data) => {
+    const { id, preview, cost, brand, name, sizes } = data;
+
+    const li = document.createElement("li");
+    li.classList.add("goods__item");
+    li.innerHTML = `
+                  <article class="good">
+                      <a class="good__link-img" href="card-good.html#${id}">
+                          <img class="good__img" src="goods-image/${preview}" alt="">
+                      </a>
+                      <div class="good__description">
+                          <p class="good__price">${cost} &#8381;</p>
+                          <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                          ${
+                            sizes
+                              ? `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(
+                                  " "
+                                )}</span></p>`
+                              : ""
+                          }
+                          
+                          <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+                      </div>
+                  </article>
+      `;
+
+    return li;
+  };
+
+  const renderGoodsList = (data) => {
+    goodsList.textContent = "";
+
+    for (const item of data) {
+      const card = createCard(item);
+      goodsList.append(card);
     }
-})
+  };
+
+  window.addEventListener('hashchange', ()=>{
+    hash = location.hash.substring(1)
+    getGoods(renderGoodsList, hash);
+  })
+
+} catch (error) {
+  console.warn(error);
+}
